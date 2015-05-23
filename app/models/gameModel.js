9d3b782a611061a297899113b908ac8686101c91
Ryan Ore <ryan@ryanore.com>
2015-05-22 19:36:08 -0500

    emergency commit

--- app/models/gameModel.js
+++ app/models/gameModel.js
@@ -1,13 +1,36 @@
+
 var mongoose  = require('mongoose'),
     Schema    = mongoose.Schema,
     uniqueValidator = require('mongoose-unique-validator');
 
 
 var GameSchema   = new Schema({
-  title: String,
-  owner: String,
-  teams: Array,
+  admins: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  description: String,
+  title: { 
+    type: String, 
+    required: true, 
+    min: 5, 
+    max: 30, 
+    unique:true, 
+    trim: true 
+  },  
+  teams: [{
+    name: {type: String},
+    score: {type: Number}
+  }],
   createdAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now }
+});
+
+
+GameSchema.pre('save', function(next){
+  now = new Date();
+  this.updatedAt = now;
+  if ( !this.createdAt ) {
+    this.createdAt = now;
+  }
+  next();
 });
 
 
@@ -16,3 +39,11 @@ module.exports = {
   schema: GameSchema,
   model: mongoose.model('Game', GameSchema)
 };
+
+
+/**
+ *
+ * Games need
+ * Owner:  the user that can update the score
+ *  
+ */
